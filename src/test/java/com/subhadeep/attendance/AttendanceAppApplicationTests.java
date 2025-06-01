deb8ad56f0a01a162b168194218259c28a0bbd09
Subhadeep Chakraborty <subhadeepc2020@gmail.com>
2025-06-01 23:04:14 +0200

    first commit

---
+++ src/test/java/com/subhadeep/attendance/AttendanceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhadeep.attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
